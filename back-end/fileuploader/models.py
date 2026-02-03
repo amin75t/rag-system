b4e745607782518d2c015f5d3093c05bdb4a92fd
Parsa Kavianpour <parsakavianpour@gmail.com>
2026-02-03 12:36:57 +0330

    Add boto3 version dependency in requirements.txt and create file upload model with one to many relationship with user entity and create utils.py in fileuploader app to manage bucket

--- back-end/fileuploader/models.py
+++ back-end/fileuploader/models.py
@@ -1,3 +1,13 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+
+class UploadedFile(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='files')
+    file_name = models.CharField(max_length=255)
+    minio_path = models.TextField()
+    file_size = models.BigIntegerField(null=True, blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.phone} - {self.file_name}"
---
+++ back-end/fileuploader/utils.py
@@ -0,0 +1,24 @@
+# file_uploader/utils.py
+import boto3
+from django.conf import settings
+
+
+def get_minio_client():
+    config = settings.MINIO_STORAGE_CONFIG
+    return boto3.client(
+        's3',
+        endpoint_url=config['ENDPOINT'],
+        aws_access_key_id=config['ACCESS_KEY'],
+        aws_secret_access_key=config['SECRET_KEY'],
+        verify=False
+    )
+
+
+def upload_to_minio(file_obj, user_id, filename):
+    s3 = get_minio_client()
+    bucket_name = settings.MINIO_STORAGE_CONFIG['BUCKET_NAME']
+
+    minio_path = f"user_{user_id}/{filename}"
+
+    s3.upload_fileobj(file_obj, bucket_name, minio_path)
+    return minio_path
